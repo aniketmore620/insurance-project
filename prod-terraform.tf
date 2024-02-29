69c86ab932a3fca59359dc30725ad10d1422e8ab
aniketmore620 <145662770+aniketmore620@users.noreply.github.com>
2024-02-29 22:17:45 +0530

    Update prod-terraform.tf

--- prod-terraform.tf
+++ prod-terraform.tf
@@ -134,7 +134,7 @@ resource "aws_eip" "proj-eip" {
 
 resource "aws_instance" "prod-server" {
   ami           = "ami-0c7217cdde317cfec"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   key_name      = "assignment"
 
   network_interface {
